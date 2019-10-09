2fccd19c9488824e950231b96180ba9d01fff600
Tolu Onasanya <tonasanya@Galatea.com>
2019-10-09 14:01:13 +0100

    added javadoc to the translation exception

--- src/main/java/org/galatea/pgbi/core/exception/TranslationException.java
+++ src/main/java/org/galatea/pgbi/core/exception/TranslationException.java
@@ -1,4 +1,7 @@
 package org.galatea.pgbi.core.exception;
 
+/**
+ * Translation Exception to be thrown when Translator classes fail to translate an input
+ */
 public class TranslationException extends Exception {
 }
